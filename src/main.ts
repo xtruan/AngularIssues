02b01b138c20e32a02e3da77d6a1f2fae3a127cc
xtruan <github@xtruan.com>
2017-05-21 18:28:27 -0400

    option to add production mode

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 // Enable production mode unless running locally
 if (!/localhost/.test(document.location.host)) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
